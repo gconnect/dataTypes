090afca05e58f26c1a221ecabe14ec307fc1b6bc
gconnect <agatevureglory@gmail.com>
2016-11-28 15:59:11 +0100

    updated files

--- app/dataTypeCode.js
+++ app/dataTypeCode.js
@@ -1,38 +1,38 @@
-module.exports = function(type_of_data){
-//function dataTypes(type_of_data){
-  if (typeof(type_of_data) === 'string'){
-    return type_of_data.length;
+module.exports= function(n){
+
+ if (typeof n === "string"){
+ return n.length;
+ }
+ if ( n === null ||n === undefined){
+   return ("no value");
+ }
+ if (typeof n === "boolean"){
+   return n;
+ }
+ if (typeof n === "number"){
+   if (n < 100 ){
+     return ("less than 100");
+   }
+   else if (n > 100){
+     return ("more than 100");
+   }
+   else{
+    return "equal to 100";
+
   }
-  if  (typeof(type_of_data) === null || type_of_data === undefined){
-      return 'no value';
-  }
-  if (typeof(type_of_data)==='boolean'){
-    return true;
-  }
-  if (typeof(type_of_data) === 'number' && type_of_data===100){
-    return 'equal to 100';
-  }
-  if (typeof(type_of_data)=== 'number' && type_of_data < 100){
-    return 'less than 100';
-  }
-  
-  if (typeof(type_of_data)=== 'number' && type_of_data > 100){
-    
-  return 'more than 100';
-  }
-  
-  if (type_of_data instanceof Array){
-    if (type_of_data[2] !=null){
-    return type_of_data[2]; 
-  }
-  else{
-    return undefined;
-  }
-  }
-      
-      
-if (typeof(type_of_data)=== 'function'){
-  return 'called callback';
-} 
-  
+ }
+   
+if(n instanceof Array){
+   if (n.length < 3){
+     return undefined;
+   }
+   else {
+     return n[2];
+   }
+ }
+ 
+ 
+ if (typeof n === "function"){
+     return n(true);
+ }
 }
